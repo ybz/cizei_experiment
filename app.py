dddd3d2ed25ffb77c4e568eec0ccb165be4cafe7
Yaniv Ben-Zaken <yaniv.ben.zaken@gmail.com>
2012-07-06 21:49:47 +0300

    Basic flask start

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
