4e3cf9caa58b8991c81ac77fc9b71d9bd110fb28
Yaniv Ben-Zaken <me@yaniv.bz>
2013-02-07 19:23:05 +0200

    add basic processing test

--- static/processing/js/test.js
+++ static/processing/js/test.js
@@ -1,1 +1,18 @@
 console.log("-- processing_js test --");
+
+function sketchProc(p) {
+    p.setup = function() {
+        p.size(200,200);
+    }
+    p.draw = function() {
+        if (p.__mousePressed) {
+            p.background(100);
+        } else {
+            p.background(0);
+        }
+    };
+}
+window.onload = function() {
+    var canvas = document.getElementById('main_canvas');
+    var processingInstance = new Processing(canvas, sketchProc);
+};
