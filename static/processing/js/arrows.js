512761f72f097f772d3eb681cf4a41aeb23a465e
Yaniv Ben-Zaken <me@yaniv.bz>
2013-02-09 20:56:02 +0200

    add processing keyboard test

---
+++ static/processing/js/arrows.js
@@ -0,0 +1,43 @@
+function sketchProc(p) {
+    p.setup = function() {
+        p.size(500,500);
+    }
+    var char_x = 40;
+    var char_y = 80;
+    p.draw = function() {
+        p.background(0, 50, 100);
+        if (p.__keyPressed && p.key == p.CODED) {
+            switch (p.keyCode) {
+                case p.RIGHT:
+                    console.log('right');
+                    char_x += 2;
+                    break;
+                case p.LEFT:
+                    console.log('left');
+                    char_x -= 2;
+                    break;
+                case p.UP:
+                    console.log('up');
+                    char_y -= 2;
+                    break;
+                case p.DOWN:
+                    console.log('down');
+                    char_y += 2;
+                    break;
+            }
+        }
+        drawCharacter(char_x, char_y);
+    };
+    function drawCharacter(x, y) {
+        p.fill(p.__keyPressed ? 100 : 0);
+        p.stroke(255);
+        p.ellipseMode('CENTER');
+        p.ellipse(x, y, 10, 10);
+        return
+    }
+}
+window.onload = function() {
+    var canvas = document.getElementById('main_canvas');
+    var processingInstance = new Processing(canvas, sketchProc);
+    window.p = processingInstance;
+};
