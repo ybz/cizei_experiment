e1efa66558cdc678bc7dbed061dc6f5d565999f7
Yaniv Ben-Zaken <me@yaniv.bz>
2013-02-14 20:01:48 +0200

    processing/cannon: (6) change cursor to crosshair

--- static/processing/js/cannon6.js
+++ static/processing/js/cannon6.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     var canvas = document.getElementById('main_canvas');
+    canvas.style.cursor = "crosshair";
     var processingInstance = new Processing(canvas);
     window.p = processingInstance;
 
