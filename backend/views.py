1ef265a6000734541bc675c3e076755534dd081e
Yaniv Ben-Zaken <me@yaniv.bz>
2013-04-19 01:52:00 +0300

    expose noc submodule through '/noc' route

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,10 @@
 from .app import app
 from flask import render_template
 
+from utils.flaskutils import route_static_path
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
+route_static_path(app, '/noc', 'noc')
