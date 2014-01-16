6e0539e4239e92890c38ca8adf126cfb85d4b47d
Yaniv Ben-Zaken <me@yaniv.bz>
2014-01-16 17:18:48 +0200

    ember: add active todos route

--- src-static/ember_todo/router.js
+++ src-static/ember_todo/router.js
@@ -1,5 +1,7 @@
 Todos.Router.map(function() {
-    this.resource('todos', { path: '/' }, function() {});
+    this.resource('todos', { path: '/' }, function() {
+        this.route('active');
+    });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
@@ -12,4 +14,16 @@ Todos.TodosIndexRoute = Ember.Route.extend({
     model: function() {
         return this.modelFor('todos');
     }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+    model: function() {
+        return this.store.filter('todo', function(todo) {
+            return !todo.get('isCompleted');
+        });
+    },
+    renderTemplate: function(controller) {
+        this.render('todos/index', {controller: controller});
+    }
+
 });
