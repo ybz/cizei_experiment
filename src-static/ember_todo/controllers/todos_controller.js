5631d63e0e5125f21c92b2341726beda13b1733e
Yaniv Ben-Zaken <me@yaniv.bz>
2014-01-16 17:32:16 +0200

    ember: make 'clear completed' button work

--- src-static/ember_todo/controllers/todos_controller.js
+++ src-static/ember_todo/controllers/todos_controller.js
@@ -16,9 +16,21 @@ Todos.TodosController = Ember.ArrayController.extend({
 
             // Sace the new model
             todo.save();
+        },
+        clearCompleted: function() {
+            var completed = this.filterBy('isCompleted', true);
+            completed.invoke('deleteRecord');
+            completed.invoce('save');
         }
-
     },
+
+    hasCompleted: function() {
+        return this.get('completed') > 0;
+    }.property('completed'),
+
+    completed: function() {
+        return this.filterBy('isCompleted', true).get('length');
+    }.property('@each.isCompleted'),
 
     remaining: function() {
         return this.filterBy('isCompleted', false).get('length');
