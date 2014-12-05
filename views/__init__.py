e1aed0e13e02d245334469249e6d54c94ad85f36
Yaniv Ben-Zaken <me@yaniv.bz>
2014-12-05 10:22:03 +0200

    add days timer

--- views/__init__.py
+++ views/__init__.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from datetime import datetime, timedelta
 
 @app.route('/favicon.ico')
 def favicon():
@@ -12,3 +13,8 @@ def index():
 @app.route('/ember_todo/')
 def ember_todo():
     return render_template('ember_todo.html')
+
+@app.route('/timer/')
+def nov_timer():
+    days_count = (datetime.now() - datetime(2014, 11, 1)).days + 1
+    return render_template('nov_timer.html', days_count=days_count)
